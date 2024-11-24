906302242fc4a1448e411341e552f580e6d14270
unknown <mohanad.13.93@gmail.com>
2024-11-24 11:45:07 +0100

    add .env file

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL","postgres://neondb_owner:LzibJtkjy69W@ep-twilight-voice-a2hn3qlm.eu-central-1.aws.neon.tech/clean_alien_glove_197535")
+os.environ.setdefault(
+    "SECRET_KEY","n5cahjj=n4d2ndk8$qd&4f3__#@slg*gd^@*0xbqeryx3)3p4$"
+)
